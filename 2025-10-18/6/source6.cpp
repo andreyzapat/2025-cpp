c82406a5aa1fb676996e3bc933c2845614be9413
andreyzapat <st115725@student.spbu.ru>
2025-10-18 22:57:52 +0300

    hw

--- 2025-10-18/6/source6.cpp
+++ 2025-10-18/6/source6.cpp
@@ -1,6 +1,23 @@
 #include <cstdio>
 int main(int argc, char** argv)
 {
-
-	return 0;
+    int n;
+    scanf("%d", &n);
+    int current = 0, max = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        int temp;
+        scanf("%d", &temp);
+        if (temp > 0) 
+        {
+            current++;
+            if (current > max) max = current;
+        }
+        else 
+        {
+            current = 0;
+        }
+    }
+    printf("%d\n", max);
+    return 0;
 }
--- 2025-10-18/7/source7.cpp
+++ 2025-10-18/7/source7.cpp
@@ -1,6 +1,17 @@
 #include <cstdio>
 int main(int argc, char** argv)
 {
-
-	return 0;
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    for (int x = -100; x <= 100; x++) 
+    {
+        long long value = ((long long)a * x + b) * x + c;
+        value = value * x + d;
+        if (value == 0) 
+        {
+            printf("%d ", x);
+        }
+    }
+    printf("\n");
+    return 0;
 }
