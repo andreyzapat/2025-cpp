0189b2e125df5a8136eb43e716d64aa19e135d04
andreyzapat <st115725@student.spbu.ru>
2025-10-29 17:12:34 +0300

    hw

--- 2025-10-04/12 source/source12.cpp
+++ 2025-10-04/12 source/source12.cpp
@@ -1,16 +1,14 @@
 #include <cstdio>
 int main(int argc, char** argv)
 {
-	long long a = 0;
-	long long b = 0;
-	scanf_s("%lld" "%lld", &a, &b);
-	if (a > b)
-	{
-		printf("%lld", a);
-	}
-	else
-	{
-		printf("%lld", b);
-	}
-	return 0;
+    long long n = 0;
+    long long m = 0;
+    scanf_s("%lld %lld", &n, &m);
+    long long ans = n * m - n - m + 2;
+    if (ans < 1)
+    {
+        ans = 1;
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
